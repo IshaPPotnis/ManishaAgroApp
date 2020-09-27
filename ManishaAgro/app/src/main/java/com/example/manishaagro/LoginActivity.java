8ac396e0a00e648d1ec72d52149bc99038f9ac57
IshaPPotnis <ishappotnis@gmail.com>
2020-09-27 13:31:16 +0530

    removed the register option from the login screen. Registration would happen from the admin end
    Signed-off-by: IshaPPotnis <ishappotnis@gmail.com>

--- ManishaAgro/app/src/main/java/com/example/manishaagro/LoginActivity.java
+++ ManishaAgro/app/src/main/java/com/example/manishaagro/LoginActivity.java
@@ -5,16 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import static com.example.manishaagro.R.id.cirLoginButton;
-import static com.example.manishaagro.R.id.text_signup;
 import static com.example.manishaagro.R.layout.activity_login;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView signUpText;
     Button ButtonCirLogin;
     EditText emlText, passText;
 
@@ -22,32 +19,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_login);
-        signUpText = findViewById(text_signup);
         ButtonCirLogin = findViewById(cirLoginButton);
         emlText = findViewById(R.id.editTextEmail);
         passText = findViewById(R.id.editTextPassword);
-        signUpText.setOnClickListener(this);
         ButtonCirLogin.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         Intent loginIntent = null;
-        switch (v.getId()) {
-            case text_signup:
-                loginIntent = new Intent(LoginActivity.this, MainActivity.class);
-                break;
-            case cirLoginButton:
-                final String employeeNameText = emlText.getText().toString().trim();
-                final String employeePasswordText = passText.getText().toString().trim();
-                if (employeeNameText.equals("Ramesh") && employeePasswordText.equals("ramesh123")) {
-                    loginIntent = new Intent(LoginActivity.this, EmployeeActivity.class);
-                    loginIntent.putExtra("Login Employee", emlText.getText().toString().trim());
-                } else if (employeeNameText.equals("Suresh") && employeePasswordText.equals("suresh123")) {
-                    loginIntent = new Intent(LoginActivity.this, ManagerActivity.class);
-                    loginIntent.putExtra("Login Manager", emlText.getText().toString().trim());
-                }
-                break;
+        if (v.getId() == cirLoginButton) {
+            final String employeeNameText = emlText.getText().toString().trim();
+            final String employeePasswordText = passText.getText().toString().trim();
+            if (employeeNameText.equals("Ramesh") && employeePasswordText.equals("ramesh123")) {
+                loginIntent = new Intent(LoginActivity.this, EmployeeActivity.class);
+                loginIntent.putExtra("Login Employee", emlText.getText().toString().trim());
+            } else if (employeeNameText.equals("Suresh") && employeePasswordText.equals("suresh123")) {
+                loginIntent = new Intent(LoginActivity.this, ManagerActivity.class);
+                loginIntent.putExtra("Login Manager", emlText.getText().toString().trim());
+            }
         }
         startActivity(loginIntent);
         finish();
--- ManishaAgro/app/src/main/java/com/example/manishaagro/MainActivity.java
+++ ManishaAgro/app/src/main/java/com/example/manishaagro/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.manishaagro;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     TextView signInText;
@@ -14,15 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        signInText =findViewById(R.id.text_signin);
-
-        signInText.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent i=new Intent(MainActivity.this,LoginActivity.class);
+            public void run() {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(i);
                 finish();
             }
-        });
+        }, 5000);
     }
 }
